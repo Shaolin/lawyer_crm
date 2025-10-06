ffd610fdb1acfa3318c6a077e6b0b79856375c94
Agozie Okolo <agozieokolo2@gmail.com>
2025-10-06 12:24:26 -0700

    Added favicon folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,12 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('crystalfavicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('crystalfavicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('crystalfavicon/favicon-16x16.png') }}">
+<link rel="manifest" href="{{ asset('crystalfavicon/site.webmanifest') }}">
+
+
     <!-- Scripts -->
     @if (app()->environment('production'))
     <link rel="stylesheet" href="{{ asset('build/assets/app-BOrthtGJ.css') }}">
