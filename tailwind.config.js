f3986890dee6c89424a51b1f6acf4bfa6c890786
Agozie Okolo <agozieokolo2@gmail.com>
2025-10-08 15:00:49 -0700

    Build Tailwind for production and update dashboard pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,22 +24,20 @@
 //     plugins: [forms],
 // }
 
+
+
 import defaultTheme from 'tailwindcss/defaultTheme'
 import forms from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: 'class',
-
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     ],
-
     theme: {
         extend: {
             fontFamily: {
@@ -47,6 +45,5 @@ export default {
             },
         },
     },
-
     plugins: [forms],
 }
